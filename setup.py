4070cc650332d5f307253b11386d7eb827fe2f1e
nir0s <nir36g@gmail.com>
2017-10-23 12:37:07 +0300

    Add Python 2 to classifiers as we now support all official Python 2's

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
