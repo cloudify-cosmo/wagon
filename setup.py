be07250318666f08d96568985477ad3e92a3d5f8
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2020-05-07 11:38:19 +0200

    Make linux_distribution optional (#135)
    * Make linux_distribution optional
    
    platform.linux_distribution doesn't exist on py3.8+, and
    distro.linux_distribution doesn't exist if distro is not installed,
    of course. This makes wagon break on import.
    
    Instead, make the import optional, and only check that the function
    is available in methods that actually use it, ie. create/repair
    (not install)
    
    * bump to 0.9.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*parts):
 
 setup(
     name='wagon',
-    version='0.9.1',
+    version='0.9.2',
     url='https://github.com/cloudify-cosmo/wagon',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
