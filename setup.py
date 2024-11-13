85769ca060d7440e2b73479fc9bdc46f6e9c45b4
krzysztofg256 <77963027+krzysztofg256@users.noreply.github.com>
2024-11-13 10:07:55 +0100

    Bump to 1.0.3 (#159)
    * Fixed security issue

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*parts):
 
 setup(
     name='wagon',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/cloudify-cosmo/wagon',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
